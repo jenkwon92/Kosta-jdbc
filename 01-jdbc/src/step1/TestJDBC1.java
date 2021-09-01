260c5f4a64b6bf440f8f68e4178b1f605e820e3e
Jen Kwon <70299766+jenkwon92@users.noreply.github.com>
2021-09-01 13:59:45 +0900

    210901

--- 01-jdbc/src/step1/TestJDBC1.java
+++ 01-jdbc/src/step1/TestJDBC1.java
@@ -2,6 +2,8 @@ package step1;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /*
@@ -21,9 +23,20 @@ public class TestJDBC1 {
 			Class.forName(driver); //Class.forName() -> class를 loading시킨다 -> static 정보가 meta space에 저장
 			System.out.println("jdbc.driver.loading");
 			//oracle database와 연결
+			//String url= "jdbc:oracle:thin:@221.150.136.4:1521:xe";
 			String url= "jdbc:oracle:thin:@127.0.0.1:1521:xe";
 			Connection con = DriverManager.getConnection(url,"scott","tiger");
 			System.out.println("oracle db와 connection : "+con); //예외가 나면 연결이 안된것
+			String sql="SELECT id,password, name, address FROM member";
+			PreparedStatement pstmt = con.prepareStatement(sql); 
+			ResultSet rs = pstmt.executeQuery(); //sql실행
+			while(rs.next()) { //조회 결과 행이 존재하면 true
+				System.out.println(rs.getString(1)+" "+rs.getString(2)+" "+rs.getString(3)+" "+rs.getString(4));
+			}
+			rs.close();
+			pstmt.close();
+			con.close();
+			
 		} catch (ClassNotFoundException e) { //ext안에 ojdbc안넣어 준경우에 exception발생
 			e.printStackTrace();
 		} catch (SQLException e) {
