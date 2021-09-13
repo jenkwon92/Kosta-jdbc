da1c5b9926407a50cebc5537f58937f12596d034
Jen Kwon <70299766+jenkwon92@users.noreply.github.com>
2021-09-13 11:27:53 +0900

    210913

---
+++ 00-db-sql/src/step8-ERD.sql
@@ -0,0 +1,26 @@
+ERD ( Entity Relationship Diagram ) 
+: 개체 관계 모델링 즉 데이터 모델링을 위한 다이어그램 
+
+db모델링은? erd
+설계는? erd, uml썼습니다.
+​
+ERWIN : ERD(entity-relationship diagram) Tool
+
+
+File -> New -> Logical/Physical 버튼 선택 
+-> Logical 데이터 모델링부터 구성 
+
+
+ERWIN 환경 설정 
+메뉴 Model - Model Propertie 선택
+Model Properties 대화상자- 세 번째 Notation탭에서 Logical Notation Physical Notation영역 모두 IE옵션 버튼을 선택
+
+
+
+Entity명
+----------
+기본키
+----------
+일반속성영역
+
+​
